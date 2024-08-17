8ff1a859a02149726aea16b9c30600ae6d7bb9bd
javi-diaz47 <javiereduardo300@gmail.com>
2024-08-17 15:42:16 -0500

    ✨ [FEAT] add yay package and flags

--- install.sh
+++ install.sh
@@ -17,15 +17,16 @@ function install_aur_pkgs() {
 }
 
 # Clone the repo
-curl -L -o dotfiles.zip https://github.com/javi-diaz47/dotfiles/archive/refs/heads/main.zip
-
-# xdg user
-install_pkgs "xdg-user-dirs"
-xdg-user-dirs-update
+#curl -L -o dotfiles.zip https://github.com/javi-diaz47/dotfiles/archive/refs/heads/main.zip
 
 # Save backup
 mv $HOME/.bashrc $HOME/.bashrc.bak
 mv $HOME/.config $HOME/.config.bak
+
+# set dotfiles
+cd ..
+mv ./dotfiles-main ./dotfiles
+cd dotfiles
 
 # Set the dotfiles
 if ! is_installed "stow"; then
@@ -45,9 +46,10 @@ base_pkgs=(
   thunar
   vlc
   fastfetch
-  #  yt-dlp
+  yt-dlp
   zathura-pdf-mupdf
   fish
+  ttf-cascadia-code-nerd
   # flameshot
 )
 
@@ -63,18 +65,15 @@ hypr_pkgs=(
   waybar
 )
 
-install_pkgs "${base_pkgs[@]}"
-install_pkgs "${hypr_pkgs[@]}"
-
 base_aur_pkgs=(
   cava
-  #  blahaj
+  blahaj
   shell-color-scripts-git
-  #  rofi-calc-git
+  rofi-calc-git
   picom-git
-  #  spotify
-  #  google-chrome
-  #  visual-studio-code-bin
+  spotify
+  google-chrome
+  visual-studio-code-bin
 )
 
 hypr_aur_pkgs=(
@@ -82,18 +81,51 @@ hypr_aur_pkgs=(
   pywal
 )
 
+# Install git
+if ! is_installed "git"; then
+  sudo pacman -S git
+fi
+
+# Install yay
+if ! is_installed "yay"; then
+  pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+fi
+
+# base packages
+install_pkgs "${base_pkgs[@]}"
 install_pkgs "${base_aur_pkgs[@]}"
+
+# hyperland packages
+install_pkgs "${hypr_pkgs[@]}"
 install_pkgs "${hypr_aur_pkgs[@]}"
 
-if 0==1; then
-  echo "[Trigger]
-  Operation = Upgrade
-  Operation = Install
-  Operation = Remove
-  Type = Package
-  Target = *
-  [Action]
-  Description = Cleaning pacman cache...
-  When = PostTransaction
-  Exec = /usr/bin/paccache -r" >/etc/pacman.d/hooks/clean_package_cache.hook
-fi
+while getopts "db" opt; do
+
+  case $opt in
+
+  d)
+    # directory
+    install_pkgs "xdg-user-dirs"
+    xdg-user-dirs-update
+    ;;
+
+  c)
+    # clean hook
+    echo "[Trigger]
+    Operation = Upgrade
+    Operation = Install
+    Operation = Remove
+    Type = Package
+    Target = *
+    [Action]
+    Description = Cleaning pacman cache...
+    When = PostTransaction
+    Exec = /usr/bin/paccache -r" >./clean.hook
+
+    sudo mkdir /etc/pacman.d/hooks
+
+    sudo mv ./clean.hook >/etc/pacman.d/hooks/clean_package_cache.hook
+    ;;
+
+  esac
+done

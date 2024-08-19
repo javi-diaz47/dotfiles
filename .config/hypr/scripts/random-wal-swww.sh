e6ede8a3fe394949988fca8e1da677e7d3786131
javi-diaz47 <javiereduardo300@gmail.com>
2024-08-19 12:36:41 -0500

    ✨ [FEAT] add wlogout and swww
    removes waypaper-engine

--- .config/hypr/scripts/random-wal-swww.sh
+++ .config/hypr/scripts/random-wal-swww.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # This file must be paste into ~/.waypaper_engine/scripts/scrips
 # -n to ignore setting the wallpaper
-wal -i $1 -n
+wal -q -i ~/wallpaper/
 
 cp ~/.cache/wal/colors-waybar.css ~/.config/waybar
 cp ~/.cache/wal/colors-cava ~/.config/cava/config
 cp ~/.cache/wal/colors-rofi-pywal.rasi ~/.config/rofi/themes/colors.rasi
+
+wallpaper=$(cat "$HOME/.cache/wal/wal")
+
+swww img $wallpaper \
+  --transition-fps=60 \
+  --transition-type=any
